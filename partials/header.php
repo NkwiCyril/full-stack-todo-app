b62faad3d91de93578b59d6afd192dae6146fb2b
NkwiCyril <akinimbomnkwi@gmail.com>
2024-04-05 13:02:05 +0100

    remove script tag

--- partials/header.php
+++ partials/header.php
@@ -7,7 +7,6 @@
   <link rel="stylesheet" href="style/index.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-  <script src="/script/index.js" ></script>
   <title>Task Manager</title>
 </head>
 
